57a8f5c92f9aad8892729aa34f7816eb37fd97f8
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2018-04-21 21:05:38 +0700

    combine two plugins into one

--- wp-content/plugins/woo-koperasi-core/woo-koperasi-core.php
+++ wp-content/plugins/woo-koperasi-core/woo-koperasi-core.php
@@ -11,10 +11,21 @@ Author URI:   github.com/keychera
 */
 
 
-function declare_koperasi_core_class() {
+function declare_koperasi_core_classes() {
+    require_once('includes/class-wc-gateway-kp-debugger.php');
+    require_once('includes/class-wc-gateway-kp-payment-bootstrapper.php');
+
     require_once('includes/class-wc-admin-kp-core.php');
+    require_once('includes/class-wc-gateway-kp-gateway.php');
 
     new WC_Admin_KP_Core_Plugin();
 }
 
-add_action( 'plugins_loaded', 'declare_koperasi_core_class', 1000);
+add_action( 'plugins_loaded', 'declare_koperasi_core_classes', 1000);
+
+function add_gateway_class( $methods ) {
+    $methods[] = 'WC_Gateway_KP_Gateway'; 
+    return $methods;
+}
+
+add_filter( 'woocommerce_payment_gateways', 'add_gateway_class' );
