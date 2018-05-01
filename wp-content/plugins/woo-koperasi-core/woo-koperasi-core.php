28e727de9b1d5be7c9037b5b4ede40ddc958e37a
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2018-05-01 17:45:39 +0700

    the plugin now checks whether woo is active or nah

--- wp-content/plugins/woo-koperasi-core/woo-koperasi-core.php
+++ wp-content/plugins/woo-koperasi-core/woo-koperasi-core.php
@@ -10,32 +10,37 @@ Author:       Kevin Erdiza
 Author URI:   github.com/keychera
 */
 
-function koperasi_core() {
-	static $core;
+/**
+ * Check if WooCommerce is active
+ **/
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    function koperasi_core() {
+        static $core;
 
-	if ( ! isset( $core ) ) {
-		require_once( 'includes/class-wc-admin-kp-core.php' );
+        if ( ! isset( $core ) ) {
+            require_once( 'includes/class-wc-admin-kp-core.php' );
 
-		$core = new WC_Admin_KP_Core_Plugin();
-	}
+            $core = new WC_Admin_KP_Core_Plugin();
+        }
 
-	return $core;
+        return $core;
+    }
+
+    function declare_koperasi_core_classes() {
+        require_once('includes/class-wc-gateway-kp-debugger.php');
+        require_once('includes/class-wc-gateway-kp-bank-bootstrapper.php');
+        require_once('includes/class-wc-user-kp-member.php');
+
+        koperasi_core();
+        require_once('includes/class-wc-gateway-kp-payment-gateway.php');
+    }
+
+    add_action( 'plugins_loaded', 'declare_koperasi_core_classes', 1000);
+
+    function add_gateway_class( $methods ) {
+        $methods[] = 'WC_Gateway_KP_Payment_Gateway'; 
+        return $methods;
+    }
+
+    add_filter( 'woocommerce_payment_gateways', 'add_gateway_class' );
 }
-
-function declare_koperasi_core_classes() {
-    require_once('includes/class-wc-gateway-kp-debugger.php');
-    require_once('includes/class-wc-gateway-kp-bank-bootstrapper.php');
-    require_once('includes/class-wc-user-kp-member.php');
-
-    koperasi_core();
-    require_once('includes/class-wc-gateway-kp-payment-gateway.php');
-}
-
-add_action( 'plugins_loaded', 'declare_koperasi_core_classes', 1000);
-
-function add_gateway_class( $methods ) {
-    $methods[] = 'WC_Gateway_KP_Payment_Gateway'; 
-    return $methods;
-}
-
-add_filter( 'woocommerce_payment_gateways', 'add_gateway_class' );
